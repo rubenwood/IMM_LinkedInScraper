61273e0323dde7f6200994200be6a850cc1f5183
N/A Ruben <ruben.wood1@gmail.com>
2023-08-02 16:06:17 +0100

    improved

--- linkedinscraper.py
+++ linkedinscraper.py
@@ -2,28 +2,46 @@
 
 import sys
 import csv
+import argparse
 from linkedin_scraper import Person, actions
 from selenium import webdriver
 
-def main(url):
+def main(url, email, password):
     driver = webdriver.Chrome()
 
-    # if email and password isn't given, it'll prompt in terminal
-    actions.login(driver)
+    actions.login(driver, email, password)
 
     person = Person(url, driver=driver)
 
     with open('output.csv', 'a', newline='') as csvfile:
-        fieldnames = ['Name', 'Experiences', 'Latest Institution', 'Location', 'Company', 'Job Title', 'URL']
+        fieldnames = ['Name', 'Latest Institution', 'Location', 'Company', 'Job Title', 'URL']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         # Only write the header once.
         if csvfile.tell() == 0:
             writer.writeheader()
 
-        writer.writerow({'Name': person.name, 'Experiences': person.experiences, 'Latest Institution': person.experiences[0].institution_name, 'Location':  person.experiences[0].location, 'Company': person.company, 'Job Title': person.job_title, 'URL': person.linkedin_url})
+        print(person)
+        
+        writer.writerow(
+            {
+                'Name': person.name, 
+                'Latest Institution': person.experiences[0].institution_name,
+                'Location': person.experiences[0].location,
+                'Company': person.company, 
+                'Job Title': person.job_title,
+                'URL':person.linkedin_url
+            }
+        )
     
     driver.quit()
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    parser = argparse.ArgumentParser(description='LinkedIn scraper.')
+    parser.add_argument('--url', type=str, help='LinkedIn profile URL.')
+    parser.add_argument('--email', type=str, help='LinkedIn login email.')
+    parser.add_argument('--password', type=str, help='LinkedIn login password.')
+
+    args = parser.parse_args()
+
+    main(args.url, args.email, args.password)
--- main_linkedin_scraper.py
+++ main_linkedin_scraper.py
@@ -1,18 +1,24 @@
 # main.py
 
 import subprocess
+import argparse
 
-def main():
+def main(email, password):
     urls = [
-        "https://www.linkedin.com/in/ali-malik-b1747965/",
-        "https://www.linkedin.com/in/nicoihezi/"
+        "https://www.linkedin.com/in/nicoihezi/",
         # Add more URLs here...
     ]
 
     for url in urls:
-        subprocess.call(['python', 'linkedinscraper.py', url])
-
-    print("=== ALL DONE ===")
+        subprocess.call(['python', 'linkedinscraper.py', '--url', url, '--email', email, '--password', password])
+    
+    print("DONE")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Main script for LinkedIn scraper.')
+    parser.add_argument('--email', type=str, help='LinkedIn login email.')
+    parser.add_argument('--password', type=str, help='LinkedIn login password.')
+
+    args = parser.parse_args()
+
+    main(args.email, args.password)
